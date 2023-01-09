70faac990e4585053ba571e4ebcc33214b526265
Ben Greanya <bengreanya@gmail.com>
2023-01-09 14:47:26 -0800

    got animal data onto page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,10 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+export default function Animal(props) {
+  return (
+    <div className="animal">
+      <img src={`${props.name}.svg`} />
+      <h2>{props.name}</h2>
+      <p>{props.says}</p>
+    </div>
+  );
 }
